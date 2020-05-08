5ad9f8239ff0ddc5f8d01e0db43f3d6b7c483e83
Chaya Greisman <chayamgreisman@gmail.com>
2020-05-08 14:25:50 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,8 +1,9 @@
 # Add your code here
 
 def help 
-  puts I accept the following commands:
-  puts 
+  puts 'I accept the following commands:'
+  puts '- help : displays this help message'
+  puts ''
 end
 
 def 
