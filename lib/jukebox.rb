1c601466f2ce26190524a23a67c856f65b006ced
Chaya Greisman <chayamgreisman@gmail.com>
2020-05-08 14:49:43 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,7 +32,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   song_choice = gets.strip
   songs.each do |each_song|
-    if song_choice == each_song or song_choice.to_i == each_song.index + 1 
+    if song_choice == each_song 
       puts "Playing #{each_song}"
     else
       puts "Invalid input, please try again"
